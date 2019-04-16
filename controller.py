9d70ea42380b732fe43ae3b6bf2ee70f25a0a63a
Edward S. Rice <erice11@unl.edu>
2019-04-15 20:20:28 -0700

    Added a controller program

---
+++ controller.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import RPi.GPIO as GPIO
+import yaml
+import time
+import sys
+import os
+from thermometer import Thermometer
+
+BASE_PATH = os.path.dirname(os.path.realpath(__file__))
+
+def basic_thermostat(relay_pin, thermometer, set_point, delay_time):
+    while True:
+        temp = thermometer.get_temperature()
+        if temp < set_point:
+            GPIO.output(relay_pin, GPIO.HIGH)
+            print('relay is ON', file=sys.stderr)
+        else:
+            GPIO.output(relay_pin, GPIO.LOW)
+            print('relay is OFF', file=sys.stderr)
+        time.sleep(delay_time)
+
+def main():
+    # load configuration file
+    config = yaml.safe_load(open(BASE_PATH + '/config.yaml', 'r'))
+
+    # set up GPIO
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(config['relay_pin'], GPIO.OUT)
+
+    thermometer = Thermometer()
+
+    try:
+        basic_thermostat(config['relay_pin'], thermometer, config['set_point'],
+                config['delay_time'])
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+
+if __name__ == '__main__':
+    try: main()
+    except Exception as e: print(e, file=sys.stderr)
+    finally: GPIO.cleanup()
